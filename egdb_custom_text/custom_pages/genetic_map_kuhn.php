ca744cf7c1ed999d03d54cb6013e97c123aae60f
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-04-05 15:48:58 +0200

    kuhn genetic map added

---
+++ egdb_custom_text/custom_pages/genetic_map_kuhn.php
@@ -0,0 +1,10 @@
+      <h1 style="font-size:26px">Genetic Map</h1>
+
+      <p class="p_font18">
+				Consensus genetic map from <a href="https://doi.org/10.3389/fpls.2017.00577" target="_blank">Genetic Map of Mango: A Tool for Mango Breeding</a> by Kuhn et al. 2017.<br>
+				seven mapping populations were used to make the consensus map.
+			</p>
+			
+			<center>
+      	<img src='<?php echo "$images_path/kuhn_genetic_map.jpeg";?>' alt='img example'>
+			</center>
--- egdb_custom_text/custom_toolbar.php
+++ egdb_custom_text/custom_toolbar.php
@@ -1,4 +1,5 @@
 <li class="nav-item"><a class="nav-link" href="/easy_gdb/custom_view.php?file_name=genome.php">Genomes</a></li>
+<li class="nav-item"><a class="nav-link" href="/easy_gdb/custom_view.php?file_name=genetic_map_kuhn.php">Map</a></li>
 
 <!-- <li class="nav-item dropdown">
   <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">More</a>
