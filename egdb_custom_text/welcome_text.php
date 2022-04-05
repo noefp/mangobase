ff6813573da90844f964986ccf33eb1696fb3b3c
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-04-05 17:52:47 +0200

    mango presentation

--- egdb_custom_text/welcome_text.php
+++ egdb_custom_text/welcome_text.php
@@ -4,7 +4,7 @@
         This site is dedicated to mango genomics, and provides multiple bioinformatics tools to explore and download omics data related to <i>Mangifera indica</i>.
       </h4>
       <p>
-        Mango is one of the top five fruit crops worldwide in terms of production (more than 55 million tons in 2018), after bananas, apples, grapes and oranges. Mangoes are an important fruit crop grown in over 103 countries across the tropical and subtropical zones. The common mango is typically a large, tropical, and evergreen tree with an upright to spreading dense canopy that can reach up to 30 m in some climates if not pruned. India is by far the largest mango producer with 41.6% of world production (18 Mt) followed by China with 10% (4.5 Mt).
+        Mango is one of the top five fruit crops worldwide in terms of production (more than 55 million tons in 2018), after bananas, apples, grapes and oranges. Mangoes are an important fruit crop grown in over 103 countries across the tropical and subtropical zones. India is by far the largest mango producer with 41.6% of world production (18 Mt) followed by China with 10% (4.5 Mt). The common mango is typically a large, tropical, and evergreen tree with an upright to spreading dense canopy that can reach up to 30 m in some climates if not pruned.
 			</p>
 			<center>
       	<img src='<?php echo "$images_path/mangobase_welcome.png";?>' width="300px" alt='img example'>
