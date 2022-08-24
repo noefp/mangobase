2d96b4f8dd80e5b8b253042a05eaf48cef4e7f94
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-08-24 16:09:41 +0200

    new configuration variables added

--- egdb_conf/easyGDB_conf.php
+++ egdb_conf/easyGDB_conf.php
@@ -1,18 +1,17 @@
 <?php
 // File paths
-// $root_path = "/home/noe/trop_db"; //use absolute path
 $root_path = "/var/www/html"; //use absolute path
-
+$egdb_files_folder = "mangobase";
 $easy_gdb_path = "$root_path/easy_gdb";
 $blast_dbs_path = "$root_path/blast_dbs";
 $expression_path = "$root_path/expression_data";
 $downloads_path = "downloads";
 $lookup_path = "$root_path/lookup"; //from root 
-$images_path = "/mangobase/egdb_images";
-$custom_text_path = "$root_path/mangobase/egdb_custom_text";
-$species_path = "$root_path/mangobase/egdb_species";
-$lab_path = "$root_path/mangobase/egdb_labs";
-$annotation_links_path = "$root_path/mangobase/annotations";
+$images_path = "/$egdb_files_folder/egdb_images";
+$custom_text_path = "$root_path/$egdb_files_folder/egdb_custom_text";
+$species_path = "$root_path/$egdb_files_folder/egdb_species";
+$lab_path = "$root_path/$egdb_files_folder/egdb_labs";
+$annotation_links_path = "$root_path/$egdb_files_folder/annotations";
 
 // header
 $dbTitle = "MangoBase";
@@ -24,6 +23,7 @@ $tb_about = 0;
 $tb_downloads = 1;
 $tb_species = 0;
 $tb_search_box = 1;
+$tb_tools = 1;
 $tb_search = 1;
 $tb_blast = 1;
 $tb_jbrowse = 1;
@@ -33,6 +33,10 @@ $tb_gene_expr = 1;
 $tb_lookup = 0;
 $tb_more = 0;
 $tb_custom = 1;
+
+// Expression Atlas
+$expr_cards = 1;
+$expr_menu = 1;
 
 // About
 $ab_citation = 1;
@@ -46,14 +50,7 @@ gene3.1";
 
 $expr_input_gene_list="gene1
 gene2
-gene3
-gene4
-gene5
-gene6
-gene7
-gene8
-gene9
-gene10";
+gene3";
 
 // Tools
 $max_lookup_input = 10000;
