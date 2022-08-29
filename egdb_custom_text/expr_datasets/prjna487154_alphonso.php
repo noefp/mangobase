c498f11414b41aec981e0a6e73e2f682c7bbfe17
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-08-29 12:57:54 +0200

    new datasets info and images

---
+++ egdb_custom_text/expr_datasets/prjna487154_alphonso.php
@@ -0,0 +1,19 @@
+<p>
+  RNA-seq of <i>Mangifera indica</i> plant tissues from cv. Alphonso
+</p>
+<h4>Experimental Conditions (1 replicate):</h4>
+  <ul>
+    <li>Root</li>
+    <li>Bark</li>
+    <li>Leaf</li>
+    <li>Flower</li>
+    <li>Peel</li>
+    <li>Pulp</li>
+    <li>Seed</li>
+  </ul>
+<p>
+  cDNA libraries were sequenced by PE sequencing (100 x 2) with Illumina Hiseq 3000 and normalized to TPM.
+  
+  This dataset was published by <a href="https://doi.org/10.1186/s13059-020-01959-8" target="_blank">Wang et al. 2020 </a> 
+  and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA487154" target="_blank">PRJNA487154</a>.
+</p>
---
+++ egdb_custom_text/expr_datasets/prjna487154_ripening.php
@@ -0,0 +1,35 @@
+<p>
+  RNA-seq of <i>Mangifera indica</i> pulp and peel on unripe and ripe stages of the varieties Hongyu, Guire-82, and Sensation.
+</p>
+<h4>Experimental Conditions (3 replicate):</h4>
+  <ul>
+    <li>Unripe peel and pulp of cv. Hongyu (Hon) and Guire-82 (Gui)</li>
+    <li>Ripe Peel and pulp of cv. Hongyu (Hon) and Guire-82 (Gui)</li>
+    <li>Peel and pulp at 94, 100, 106 and 112 days after pollination (DAP) of cv. Sensation</li>
+  </ul>
+<p>
+  94, 100, 106 and 112 DAP are equivalent to 2, 8, 14, 20 days not-bagged fruit.
+  <br>
+  <br>
+  cDNA libraries were sequenced by PE sequencing (100 x 2) with Illumina Hiseq 3000 and normalized to TPM.
+  
+  This dataset was published by <a href="https://doi.org/10.1186/s13059-020-01959-8" target="_blank">Wang et al. 2020 </a> 
+  and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA487154" target="_blank">PRJNA487154</a>.
+</p>
+
+<img class='rounded' src='<?php echo "$images_path/expr/hongyu_unripe.jpeg";?>' width="180px" alt='mango fruit'>
+<img class='rounded' src='<?php echo "$images_path/expr/hongyu_ripe.jpeg";?>' width="180px" alt='mango fruit'>
+<img class='rounded' src='<?php echo "$images_path/expr/guire_unripe.jpeg";?>' width="180px" alt='mango fruit'>
+<img class='rounded' src='<?php echo "$images_path/expr/guire_ripe.jpeg";?>' width="180px" alt='mango fruit'>
+<img class='rounded' src='<?php echo "$images_path/expr/sensation_94dap.jpeg";?>' width="180px" alt='mango fruit'>
+<img class='rounded' src='<?php echo "$images_path/expr/sensation_100dap.jpeg";?>' width="180px" alt='mango fruit'>
+<img class='rounded' src='<?php echo "$images_path/expr/sensation_106dap.jpeg";?>' width="180px" alt='mango fruit'>
+<img class='rounded' src='<?php echo "$images_path/expr/sensation_112dap.jpeg";?>' width="180px" alt='mango fruit'>
+<br>
+<p>Unripe and ripe stages of fruits of the varieties Hongyu, and Guire-82, and fruits at 94, 100, 106 and 112 days after pollination (DAP) of cv. Sensation</p>
+<br>
+<br>
+
+
+
+<b>DAP</b> = Days After pollination?
