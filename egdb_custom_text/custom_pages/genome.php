efff768890fc3e4250982c78bc4aff3fe96feee6
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-08-30 13:51:15 +0200

    information updated

--- egdb_custom_text/custom_pages/genome.php
+++ egdb_custom_text/custom_pages/genome.php
@@ -1,30 +1,35 @@
       <h1 style="font-size:26px">Genomes</h1>
 
       <h4 class="p_font18">
-        Currently there are several mango genome sequences available. Data are available in the <a href="/easy_gdb/downloads.php">Downloads section.</a>
+        Currently, there are several mango genome sequences available. 
+        Sequence and annotation data are available in the <a href="/easy_gdb/downloads.php">Downloads section</a>.
       </h4>
       <p>
 				<ul>
-					<li><a href="https://doi.org/10.1186/s12870-021-02858-1">Tommy Atkins by the Mango Genome Consortium</a></li>
-					<li><a href="https://doi.org/10.1186/s13059-020-01959-8">Alphonso by Wang et al.</a></li>
+					<li>cv. Tommy Atkins, by the <a href="https://doi.org/10.1186/s12870-021-02858-1" target="_blank">Mango Genome Consortium 2021</a></li>
+					<li>cv. Alphonso, by <a href="https://doi.org/10.1186/s13059-020-01959-8" target="_blank">Wang et al. 2020</a> Available at the <a href="https://www.ncbi.nlm.nih.gov/data-hub/genome/GCF_011075055.1/" target="_blank"> NCBI</a>.</li>
+					<li>cv. Irwin, by <a href="https://doi.org/10.3389/fpls.2021.749108" target="_blank">Ma et al. 2021</a> Only raw data available at the <a href="https://ngdc.cncb.ac.cn/search/?dbId=&q=CRA004336" target="_blank"> NGDC CNCB</a>.</li>
+					<li>cv. Hong Xiang Ya, by <a href="https://doi.org/10.1101/2020.02.22.960880" target="_blank">Li et al. 2020</a> Available at the <a href="https://ngdc.cncb.ac.cn/gwh/Assembly/967/show" target="_blank"> NGDC CNCB Genome Warehouse</a>.</li>
 				</ul>
       </p>
       <p>
         Another genome version, from the cultivar Amrapali, was also sequences but is not published yet. The genome assembly information can be found at the NCBI:
 				<ul>
-					<li><a href="https://www.ncbi.nlm.nih.gov/assembly/GCA_020138855.1/"><i>M.indica</i> Amrapali 3.0</a></li>
-					<li><a href="https://www.ncbi.nlm.nih.gov/assembly/GCA_021014495.1/"><i>M.indica</i> Neelam pat 1.0</a></li>
-					<li><a href="https://www.ncbi.nlm.nih.gov/assembly/GCA_021014955.1/"><i>M.indica</i> Dashehari mat 1.0</a></li>
+					<li><a href="https://www.ncbi.nlm.nih.gov/assembly/GCA_020138855.1/" target="_blank"><i>M.indica</i> Amrapali 3.0</a></li>
+					<li><a href="https://www.ncbi.nlm.nih.gov/assembly/GCA_021014495.1/" target="_blank"><i>M.indica</i> Neelam pat 1.0</a></li>
+					<li><a href="https://www.ncbi.nlm.nih.gov/assembly/GCA_021014955.1/" target="_blank"><i>M.indica</i> Dashehari mat 1.0</a></li>
 				</ul>
       </p>
       
       <h2>Genome stats</h2>
-      <table class="table table-bordered" style="width:600px">
+      <table class="table table-bordered" style="width:700px">
          <thead>
            <tr>
              <th></th>
              <th>Tommy Atkins</th>
              <th>Alphonso</th>
+             <th>Irwin</th>
+             <th>Hong Xiang Ya</th>
            </tr>
          </thead>
          <tbody>
@@ -32,31 +37,42 @@
              <td>Number of sequences</td>
              <td class="text-right">21</td>
              <td class="text-right">250</td>
+             <td class="text-right">1305</td>
+             <td class="text-right">28</td>
            </tr>
            <tr>
              <td>Assembly size (Mb)</td>
              <td class="text-right">377.29</td>
              <td class="text-right">391.96</td>
+             <td class="text-right">396.19</td>
+             <td class="text-right">371.62</td>
            </tr>
            <tr>
              <td>Longest sequence (Mb)</td>
              <td class="text-right">48.59</td>
              <td class="text-right">29.46</td>
+             <td class="text-right">13.15</td>
+             <td class="text-right">27.76</td>
            </tr>
            <tr>
              <td>Average length (Mb)</td>
              <td class="text-right">17.97</td>
              <td class="text-right">15.68</td>
+             <td class="text-right">0.30</td>
+             <td class="text-right">13.27</td>
            </tr>
            <tr>
              <td>N50 length (Mb)</td>
              <td class="text-right">17.32</td>
              <td class="text-right">17.65</td>
+             <td class="text-right">1.03</td>
+             <td class="text-right">18.78</td>
            </tr>
            <tr>
              <td>N50 index (sequences)</td>
              <td class="text-right">8</td>
              <td class="text-right">10</td>
+             <td class="text-right">-</td>
            </tr>
          </tbody>
        </table>
