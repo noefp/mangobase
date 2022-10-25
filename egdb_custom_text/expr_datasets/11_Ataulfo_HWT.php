61a015d18301c31b38463167c6657f4886784446
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-10-25 17:30:58 +0200

    dataset descriptions corrected

--- egdb_custom_text/expr_datasets/11_Ataulfo_HWT.php
+++ egdb_custom_text/expr_datasets/11_Ataulfo_HWT.php
@@ -1,5 +1,5 @@
 <p>
-  RNA-seq of <i>Mangifera indica</i> (cultivar Ataulfo) pulp after 1 and 10 days following hot water brush (HWB) post-harvest treatment and their controls (no post-harvest treatment). Day 1 is unripe stage and day 10 is the ripe stage.
+  RNA-seq of <i>Mangifera indica</i> (cultivar Ataulfo) pulp after 1 (unripe) and 10 (ripe) days following hot water brush (HWB) post-harvest treatment and their controls.
 </p>
 <h4>Experimental Conditions (2 replicates):</h4>
   <ul>
@@ -7,7 +7,7 @@
     <li>Pulp 1 and 10 days following post-harvest hot water treatment.</li>
   </ul>
 <p>
-  Mature-green stage mango (Mangifera indica L.) fruit cultivar “Ataulfo” were obtained at the Diazteca packing facility in Escuinapa, Sinaloa, México, where they were subjected to the United States Agricultural Department APHIS hot water treatment (HWT). cDNA libraries were sequenced by PE sequencing (100 x 2) with Illumina Genome Analyzer IIx and normalized to TPM. This dataset was published by <a href="https://doi.org/10.1016/j.scienta.2017.09.031" target="_blank">Dautt-Castro et al. 2018</a> and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA286253" target="_blank">PRJNA286253</a>.
+  Mature-green mango fruits cv. Ataulfo were obtained at the Diazteca packing facility in Escuinapa, Sinaloa, México, where they were subjected to the United States Agricultural Department APHIS hot water treatment (HWT). cDNA libraries were sequenced by PE sequencing (100 x 2) with Illumina Genome Analyzer IIx and normalized to TPM. This dataset was published by <a href="https://doi.org/10.1016/j.scienta.2017.09.031" target="_blank">Dautt-Castro et al. 2018</a>. Raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA286253" target="_blank">PRJNA286253</a>.
 </p>
 
 <img class='rounded' src='<?php echo "$images_path/expr/ata_pulp_unripe_ctr.jpeg";?>' width="180px" alt='Ataulfo pulp'>
--- egdb_custom_text/expr_datasets/12_shelly_Colletotrichum.php
+++ egdb_custom_text/expr_datasets/12_shelly_Colletotrichum.php
@@ -1,23 +1,23 @@
 <p>
-  RNA-seq of <i>Mangifera indica</i> (cultivar Shelly) from the peel of the red and green sides of the red and green fruit inoculated with C. gloeosporioides at time 0, 2, 7 days post inoculation.
+  RNA-seq of <i>Mangifera indica</i> (cultivar Shelly) from the peel of the red and green sides of the red and green fruit inoculated with <i>C. gloeosporioides</i> at time 0, 2, 7 days post inoculation.
 </p>
 <h4>Experimental Conditions (3 replicates):</h4>
   <ul>
-    <li>Green Fruit Green Side at 0, 2, 7 days post inoculation (dpi).</li>
-    <li>Green Fruit Red Side at 0, 2, 7 dpi.</li>
-    <li>Red Fruit Green Side at 0, 2, 7 dpi.</li>
-    <li>Red Fruit Red Side at 0, 2, 7 dpi.</li>
+    <li>Peel from Green Fruit Green Side at 0, 2, 7 days post inoculation (dpi).</li>
+    <li>Peel from Green Fruit Red Side at 0, 2, 7 dpi.</li>
+    <li>Peel from Red Fruit Green Side at 0, 2, 7 dpi.</li>
+    <li>Peel from Red Fruit Red Side at 0, 2, 7 dpi.</li>
   </ul>
 <p>
-  Mango (Mangifera indica L., cv. Shelly) fruit were harvested in July 2017. The fruit were picked from two different positions of the canopy in the orchard: the ‘red’ colored fruit (RF) from the exterior position with direct exposure to sunlight, and the ‘green’ colored fruit (GF) from the interior area in the shaded part of the canopy. cDNA libraries were sequenced by SE sequencing with Illumina HiSeq 2500 and normalized to TPM. This dataset was published by<a href="https://academic.oup.com/hr/article/doi/10.1038/s41438-020-00452-4/6446603" target="_blank">Sudheeran PK et al. 2021</a> and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA575336" target="_blank">PRJNA575336 </a>.
+  Mango fruits were harvested in July 2017. The fruit were picked from two different positions of the canopy in the orchard: the ‘red’ colored fruit (RF) from the exterior position with direct exposure to sunlight, and the ‘green’ colored fruit (GF) from the interior area in the shaded part of the canopy. cDNA libraries were sequenced by SE sequencing with Illumina HiSeq 2500 and normalized to TPM. This dataset was published by <a href="https://academic.oup.com/hr/article/doi/10.1038/s41438-020-00452-4/6446603" target="_blank">Sudheeran PK et al. 2021</a>. Raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA575336" target="_blank">PRJNA575336 </a>.
 </p>
 
-<img class='rounded' src='<?php echo "$images_path/expr/gf_0dpi.jpeg";?>' width="180px" alt='shelly pulp'>
-<img class='rounded' src='<?php echo "$images_path/expr/gfgs_12dpi.jpeg";?>' width="180px" alt='shelly pulp'>
-<img class='rounded' src='<?php echo "$images_path/expr/gfrs_12dpi.jpeg";?>' width="180px" alt='shelly pulp'>
-<img class='rounded' src='<?php echo "$images_path/expr/rf_0dpi.jpeg";?>' width="180px" alt='shelly pulp'>
-<img class='rounded' src='<?php echo "$images_path/expr/rfgs_12dpi.jpeg";?>' width="180px" alt='shelly pulp'>
-<img class='rounded' src='<?php echo "$images_path/expr/rfrs_12dpi.jpeg";?>' width="180px" alt='shelly pulp'>
+<img class='rounded' src='<?php echo "$images_path/expr/gf_0dpi.jpeg";?>' width="180px" alt='shelly peel'>
+<img class='rounded' src='<?php echo "$images_path/expr/gfgs_12dpi.jpeg";?>' width="180px" alt='shelly peel'>
+<img class='rounded' src='<?php echo "$images_path/expr/gfrs_12dpi.jpeg";?>' width="180px" alt='shelly peel'>
+<img class='rounded' src='<?php echo "$images_path/expr/rf_0dpi.jpeg";?>' width="180px" alt='shelly peel'>
+<img class='rounded' src='<?php echo "$images_path/expr/rfgs_12dpi.jpeg";?>' width="180px" alt='shelly peel'>
+<img class='rounded' src='<?php echo "$images_path/expr/rfrs_12dpi.jpeg";?>' width="180px" alt='shelly peel'>
 <p>
   From left to right, green mango fruit 0 days post inoculation (dpi), green fruit green side (GFGS) 12 dpi, green fruit red side (GFRS) 12 dpi, red mango fruit 0 dpi, red fruit green side (RFGS) 12 dpi, and red fruit red side (RFRS) 12 dpi.
 </p>
