380a19c5b3462fe6e42abe7fb9bbe37c271b8aa5
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-10-25 17:23:39 +0200

    new dataset descriptions

---
+++ egdb_custom_text/expr_datasets/11_Ataulfo_HWT.php
@@ -0,0 +1,26 @@
+<p>
+  RNA-seq of <i>Mangifera indica</i> (cultivar Ataulfo) pulp after 1 and 10 days following hot water brush (HWB) post-harvest treatment and their controls (no post-harvest treatment). Day 1 is unripe stage and day 10 is the ripe stage.
+</p>
+<h4>Experimental Conditions (2 replicates):</h4>
+  <ul>
+    <li>Pulp 1 and 10 days controls</li>
+    <li>Pulp 1 and 10 days following post-harvest hot water treatment.</li>
+  </ul>
+<p>
+  Mature-green stage mango (Mangifera indica L.) fruit cultivar “Ataulfo” were obtained at the Diazteca packing facility in Escuinapa, Sinaloa, México, where they were subjected to the United States Agricultural Department APHIS hot water treatment (HWT). cDNA libraries were sequenced by PE sequencing (100 x 2) with Illumina Genome Analyzer IIx and normalized to TPM. This dataset was published by <a href="https://doi.org/10.1016/j.scienta.2017.09.031" target="_blank">Dautt-Castro et al. 2018</a> and raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA286253" target="_blank">PRJNA286253</a>.
+</p>
+
+<img class='rounded' src='<?php echo "$images_path/expr/ata_pulp_unripe_ctr.jpeg";?>' width="180px" alt='Ataulfo pulp'>
+<img class='rounded' src='<?php echo "$images_path/expr/ata_pulp_unripe_hwt.jpeg";?>' width="180px" alt='Ataulfo pulp'>
+<img class='rounded' src='<?php echo "$images_path/expr/ata_pulp_ripe_ctr.jpeg";?>' width="180px" alt='Ataulfo pulp'>
+<img class='rounded' src='<?php echo "$images_path/expr/ata_pulp_ripe_hwt.jpeg";?>' width="180px" alt='Ataulfo pulp'>
+<p>
+  From left to right, mango fruits and pulp (below) in control conditions after 1 day post-harvest (unripe), fruits and pulp after hot water treatment (HWT) and 1 day post-harvest, fruits and pulp in control conditions and 10 days post-harvest (ripe), fruits and pulp after HWT and 10 days post-harvest.
+</p>
+<p>
+  <b>Ata</b> = Ataulfo
+  <br>
+  <b>CTR</b> = Control
+  <br>
+  <b>HWT</b> = Hot water treatment
+</p>
