48f2320fc3bce3ed91811dff6f919063a619a8cb
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-02-06 15:44:20 +0100

    paper citation added

--- egdb_custom_text/db_citation.php
+++ egdb_custom_text/db_citation.php
@@ -1,13 +1,18 @@
 <div class="row">
   <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 
-    <h4>Please, cite the next reference when using this web site or the Tommy Atkins mango genome data:</h4>
+    <h4>Please, cite the next references when using this web site or the Tommy Atkins mango genome data respectively:</h4>
 
     <div class="card bg-light">
-      <div class= "card-body">    
+      <div class= "card-body" style="padding-bottom:0px">
+        Gómez-Ollé A, Bullones A, Hormaza JI, Mueller LA and Fernandez-Pozo N (2023).
+        <b>MangoBase: a genomics portal and gene expression atlas for <i>Mangifera indica</i></b><br>
+        <a href="#" target="blank">Plants. Submitted</a>
+      </div>
+      <div class= "card-body">
         Mango Genome Consortium, Bally ISE, Bombarely A, Chambers AH, Cohen Y, Dillon NL, Innes DJ, Islas-Osuna MA, Kuhn DN, Mueller LA, Ophir R, Rambani A, Sherman A, Yan H. (2021).
         <b>The 'Tommy Atkins' mango genome reveals candidate genes for fruit quality</b><br>
-        <a href="https://doi.org/10.1186/s12870-021-02858-1" target="blank">BMC Plant Biology 21, Article number: 108</a><br>
+        <a href="https://doi.org/10.1186/s12870-021-02858-1" target="blank">BMC Plant Biology 21, Article number: 108</a>
       </div>
     </div>
 
