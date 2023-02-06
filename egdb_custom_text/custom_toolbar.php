280b29ca8ccb17e949aabd6929a9304ae1928364
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-02-06 15:55:11 +0100

    reorganized toolbar

--- egdb_custom_text/custom_toolbar.php
+++ egdb_custom_text/custom_toolbar.php
@@ -1,12 +1,9 @@
-<li class="nav-item"><a class="nav-link" href="/easy_gdb/custom_view.php?file_name=genome.php">Genomes</a></li>
-<li class="nav-item"><a class="nav-link" href="/easy_gdb/custom_view.php?file_name=genetic_map_kuhn.php">Map</a></li>
-
-<!-- <li class="nav-item dropdown">
-  <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">More</a>
+<li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Gene expression</a>
   <div class="dropdown-menu">
-    <a class="dropdown-item" href="/easy_gdb/custom_view.php?file_name=other page.php">other page</a>
-    <a class="dropdown-item" href="/easy_gdb/custom_view.php?file_name=table_to_page.php&table_name=tables/Table Example.txt&link_field=ACC Name" >Table Example</a>
-    <a class="dropdown-item" href="/easy_gdb/custom_view.php?file_name=table_menu.php">Table Menu</a>
+    <a class="dropdown-item" href="/easy_gdb/tools/expression/expression_input.php">Expression viewer</a>
+    <a class="dropdown-item" href="/easy_gdb/tools/expression/expression_menu.php">Datasets</a>
   </div>
-</li> -->
-
+</li>
+<li class="nav-item"><a class="nav-link" href="/easy_gdb/custom_view.php?file_name=genome.php">Genomes</a></li>
+<li class="nav-item"><a class="nav-link" href="/easy_gdb/custom_view.php?file_name=genetic_map_kuhn.php">Maps</a></li>
