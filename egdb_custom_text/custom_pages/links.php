cee93a9d89a1f610d2c0e3e1a56dc6328ecaf73b
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-02-27 09:49:37 +0100

    links of interest page

---
+++ egdb_custom_text/custom_pages/links.php
@@ -0,0 +1,8 @@
+<div class="width900">
+  <h1 style="font-size:26px">Links of interest</h1>
+  <br>
+  <div class="list-group">
+    <a href="http://webtom.cabgrid.res.in/mangosnps/" class="list-group-item list-group-item-action" target="_blank">Mango SNP Database</a>
+    <a href="http://www.mangifera.org/index.php" class="list-group-item list-group-item-action" target="_blank">Mango Resources Information System</a>
+  </div>
+</div>
