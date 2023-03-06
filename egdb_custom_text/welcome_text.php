d2625e717fe9159a4057843dc3e20e423b2d2a94
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-03-06 16:19:13 +0100

    contact link added

--- egdb_custom_text/welcome_text.php
+++ egdb_custom_text/welcome_text.php
@@ -7,6 +7,11 @@
 Mango (<i>Mangifera indica</i> L.) (2n = 40) is a member of the Anacardiaceae family, which belong to Mangifera, a genus with between 31 and 69 species, of which 26 produce edible fruits. Mangoes are delicious fruits with great nutritional value. They are one of the major fruit crops worldwide, ranking fifth in terms of production among perennial fruit crops worldwide, and currently cultivated in more than 100 countries, with a production of more than 40 million tons in 2021 (FAO-STAT). India, China, Thailand, Indonesia, and Mexico are the main mango producing countries, although this crop is cultivated in tropical and subtropical regions of all continents but Antarctica, including regions far from the equator, such as the Mediterranean basin in the south of Europe, north of Africa and the Middle East. Mango was domesticated at least 4000 years ago in Asia, where two ecogeographic races of mango are distinguished: the Indian type, in subtropical regions, which produces monoembryonic seeds, and the Southeast Asian type, in tropical regions, which produces polyembryonic seeds.
 </p>
 
+<p>
+  Please, <a id="contact_link" onclick="contt()"> <i class="fa fa-envelope" style="font-size:24px;color:#007bff"></i> Contact us</a>
+	if you have any question or request.
+</p>
+
 
 <div class="card">
   <div class="card-body">
@@ -21,3 +26,18 @@ Mango (<i>Mangifera indica</i> L.) (2n = 40) is a member of the Anacardiaceae fa
 
 <br>
 
+<script>
+  function contt() {
+      var addr = "e_fernandez_pozo" + '>' + "csic_es";
+      addr = addr.replace(/_/g, ".");
+      addr = addr.replace(">", "@");
+      //alert("addr: no"+addr);
+      window.location.href='mailto:no'+addr+'?subject=MangoBase contact';
+  }
+</script>
+<style>
+  #contact_link {
+    color: #007bff;
+    cursor: pointer;
+  }
+</style>
