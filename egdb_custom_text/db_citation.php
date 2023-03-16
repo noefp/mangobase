469b1b167450ff374b0922108130e80f10bbf2a5
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-03-16 13:23:06 +0100

    updated citation

--- egdb_custom_text/db_citation.php
+++ egdb_custom_text/db_citation.php
@@ -7,12 +7,12 @@
       <div class= "card-body" style="padding-bottom:0px">
         Gómez-Ollé A, Bullones A, Hormaza JI, Mueller LA and Fernandez-Pozo N (2023).
         <b>MangoBase: a genomics portal and gene expression atlas for <i>Mangifera indica</i></b><br>
-        <a href="#" target="blank">Plants. Submitted</a>
+        <a href="https://doi.org/10.3390/plants12061273" target="_blank">Plants 2023, 12(6), 1273</a>
       </div>
       <div class= "card-body">
         Mango Genome Consortium, Bally ISE, Bombarely A, Chambers AH, Cohen Y, Dillon NL, Innes DJ, Islas-Osuna MA, Kuhn DN, Mueller LA, Ophir R, Rambani A, Sherman A, Yan H. (2021).
         <b>The 'Tommy Atkins' mango genome reveals candidate genes for fruit quality</b><br>
-        <a href="https://doi.org/10.1186/s12870-021-02858-1" target="blank">BMC Plant Biology 21, Article number: 108</a>
+        <a href="https://doi.org/10.1186/s12870-021-02858-1" target="_blank">BMC Plant Biology 21, Article number: 108</a>
       </div>
     </div>
 
