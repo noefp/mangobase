4a3fd8c97ed60683eebc62942addf06a8fedc1d1
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-03-20 15:52:26 +0100

    links to PDFs added

--- egdb_custom_text/custom_pages/links.php
+++ egdb_custom_text/custom_pages/links.php
@@ -4,5 +4,8 @@
   <div class="list-group">
     <a href="http://webtom.cabgrid.res.in/mangosnps/" class="list-group-item list-group-item-action" target="_blank">Mango SNP Database</a>
     <a href="http://www.mangifera.org/index.php" class="list-group-item list-group-item-action" target="_blank">Mango Resources Information System</a>
+    <a href="/downloads/PDFs/descriptors_mango.pdf" class="list-group-item list-group-item-action"><i class='far fa-file-pdf' style='font-size:30px;color:red'></i> Descriptors for mango</a>
+    <a href="/downloads/PDFs/Mango-Booklet-English.pdf" class="list-group-item list-group-item-action"><i class='far fa-file-pdf' style='font-size:30px;color:red'></i> Training and pruning the mango tree</a>
+    <a href="/downloads/PDFs/mango_guia_plagas.pdf" class="list-group-item list-group-item-action"><i class='far fa-file-pdf' style='font-size:30px;color:red'></i> Guía de gestión integrada de plagas de mango</a>
   </div>
 </div>
