8ce81b1817351a9bb047b962b6f9e011a812fc1a
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2024-03-22 10:10:08 +0100

    added Mango Board and link icons

--- egdb_custom_text/custom_pages/links.php
+++ egdb_custom_text/custom_pages/links.php
@@ -2,11 +2,13 @@
   <h1 style="font-size:26px">Links of interest</h1>
   <br>
   <div class="list-group">
-    <a href="https://office.fruttmac.com/wiki" class="list-group-item list-group-item-action" target="_blank">ICIA Mango Germplasm Collection</a>
-    <a href="http://webtom.cabgrid.res.in/mangosnps/" class="list-group-item list-group-item-action" target="_blank">Mango SNP Database</a>
+    <a href="https://www.mango.org/" class="list-group-item list-group-item-action" target="_blank"><i class='fas fa-external-link-alt' style='font-size:24px;color:blue'></i> US National Mango Board</a>
+    <a href="https://office.fruttmac.com/wiki" class="list-group-item list-group-item-action" target="_blank"><i class='fas fa-external-link-alt' style='font-size:24px;color:blue'></i> ICIA Mango Germplasm Collection</a>
+    <a href="http://webtom.cabgrid.res.in/mangosnps/" class="list-group-item list-group-item-action" target="_blank"><i class='fas fa-external-link-alt' style='font-size:24px;color:blue'></i> Mango SNP Database</a>
     <!-- <a href="http://www.mangifera.org/index.php" class="list-group-item list-group-item-action" target="_blank">Mango Resources Information System</a> -->
     <a href="/downloads/PDFs/descriptors_mango.pdf" class="list-group-item list-group-item-action"><i class='far fa-file-pdf' style='font-size:30px;color:red'></i> Descriptors for mango</a>
     <a href="/downloads/PDFs/Mango-Booklet-English.pdf" class="list-group-item list-group-item-action"><i class='far fa-file-pdf' style='font-size:30px;color:red'></i> Training and pruning the mango tree</a>
     <a href="/downloads/PDFs/mango_guia_plagas.pdf" class="list-group-item list-group-item-action"><i class='far fa-file-pdf' style='font-size:30px;color:red'></i> Guía de gestión integrada de plagas de mango</a>
   </div>
 </div>
+
