91ee51cfbaa3df0eebadcba93c8b42215b7fd23d
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2024-03-22 11:01:11 +0100

    added new Irwin genome

--- egdb_custom_text/custom_pages/genome.php
+++ egdb_custom_text/custom_pages/genome.php
@@ -6,6 +6,7 @@
       </h4>
       <p>
 				<ul>
+					<li>cv. Irwin, by  <a href="https://doi.org/10.1002/tpg2.20441" target="_blank">Wijesundara et al. 2024</a></li>
 					<li>cv. Tommy Atkins, by the <a href="https://doi.org/10.1186/s12870-021-02858-1" target="_blank">Mango Genome Consortium 2021</a></li>
 					<li>cv. Alphonso, by <a href="https://doi.org/10.1186/s13059-020-01959-8" target="_blank">Wang et al. 2020</a> Available at the <a href="https://www.ncbi.nlm.nih.gov/data-hub/genome/GCF_011075055.1/" target="_blank"> NCBI</a>.</li>
 					<li>cv. Irwin, by <a href="https://doi.org/10.3389/fpls.2021.749108" target="_blank">Ma et al. 2021</a> Only raw data available at the <a href="https://ngdc.cncb.ac.cn/search/?dbId=&q=CRA004336" target="_blank"> NGDC CNCB</a>.</li>
@@ -26,22 +27,25 @@
          <thead>
            <tr>
              <th></th>
+             <th>Irwin 2024</th>
              <th>Tommy Atkins</th>
              <th>Alphonso</th>
-             <th>Irwin</th>
+             <th>Irwin 2021</th>
              <th>Hong Xiang Ya</th>
            </tr>
          </thead>
          <tbody>
            <tr>
              <td>Number of sequences</td>
-             <td class="text-right">21</td>
+             <td class="text-right">23</td>
+             <td class="text-right">571</td>
              <td class="text-right">250</td>
              <td class="text-right">1305</td>
-             <td class="text-right">28</td>
+             <td class="text-right">120</td>
            </tr>
            <tr>
              <td>Assembly size (Mb)</td>
+             <td class="text-right">365</td>
              <td class="text-right">377.29</td>
              <td class="text-right">391.96</td>
              <td class="text-right">396.19</td>
@@ -49,31 +53,36 @@
            </tr>
            <tr>
              <td>Longest sequence (Mb)</td>
-             <td class="text-right">48.59</td>
+             <td class="text-right">28.79</td>
+             <td class="text-right">22.40</td>
              <td class="text-right">29.46</td>
              <td class="text-right">13.15</td>
-             <td class="text-right">27.76</td>
+             <td class="text-right">11.47</td>
            </tr>
            <tr>
              <td>Average length (Mb)</td>
-             <td class="text-right">17.97</td>
-             <td class="text-right">15.68</td>
+             <td class="text-right">15.85</td>
+             <td class="text-right">0.66</td>
+             <td class="text-right">1.56</td>
              <td class="text-right">0.30</td>
-             <td class="text-right">13.27</td>
+             <td class="text-right">3.10</td>
            </tr>
            <tr>
-             <td>N50 length (Mb)</td>
-             <td class="text-right">17.32</td>
-             <td class="text-right">17.65</td>
+             <td>Contig N50 length (Mb)</td>
+             <td class="text-right">14.98</td>
+             <td class="text-right">0.04</td>
+             <td class="text-right">3.50</td>
              <td class="text-right">1.03</td>
-             <td class="text-right">18.78</td>
+             <td class="text-right">4.82</td>
            </tr>
-           <tr>
+           <!-- <tr>
              <td>N50 index (sequences)</td>
-             <td class="text-right">8</td>
+             <td class="text-right">-</td>
+             <td class="text-right">16</td>
              <td class="text-right">10</td>
              <td class="text-right">-</td>
-           </tr>
+             <td class="text-right">-</td>
+           </tr> -->
          </tbody>
        </table>
 
